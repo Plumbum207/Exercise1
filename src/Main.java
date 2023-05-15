12594dbc23e56c16836397817dc738c7a619f226
Vladimir <plumbumtrident@gmail.com>
2023-05-16 00:09:11 +0700

    Задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+// Напишите программу, которая проверяет, является ли строка палиндромом.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = "tenet";
+        System.out.println(isPalindrome(input));
+    }
+
+    public static boolean isPalindrome(String input) {
+
+        int length = input.length();
+
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) != input.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
